dba84b29ccd5ed5046570001eecd228604786e9a
chandan kumar <ck751515@gmail.com>
2020-08-27 19:51:23 +0530

    initial commit

---
+++ config/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+    'development':{
+      'secret': 'supersecret',
+      'connectionString':`mongodb://localhost:27017/api-db`
+    },
+    'test':{
+        'secret': 'supersecret',
+        'connectionString':`conncetion string`
+    },
+    'production':{
+        'secret': 'supersecret',
+        'connectionString':`conncetion string`
+    }
+    
+  };
