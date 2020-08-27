85072e10e2f5f9701b75e0440d02c3a2df4f27ea
chandan kumar <ck751515@gmail.com>
2020-08-27 19:51:50 +0530

    initial commit

---
+++ utils/HTTPMsg.js
@@ -0,0 +1,12 @@
+module.exports = {
+    
+    created : param => `${param} created successfully`,
+    ok: `ok`,
+    badRequest:`Request not valid`,
+    serverError:`something went wrong!`,
+    passwordMismatch:`paassword doesn't match with confirm password`,
+    notFound:` not found`,
+    invalidCredential:`invalid credentials`,
+    exists:param => `${param} exists`
+    
+}
