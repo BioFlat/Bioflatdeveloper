b78c8ba068617432965ed66b34b9bfab0a53e183
chandan kumar <ck751515@gmail.com>
2020-09-05 10:44:41 +0530

    initial commit

--- api/index.js
+++ api/index.js
@@ -1,9 +1,20 @@
 const router = require('express').Router(); 
 const auth = require('./authentication');
-const course = require('./authentication');
-
+const Store = require('./store');
+const Address = require("./address")
+const Product = require("./product")
+const Price = require('./price')
+const Cart = require("./cart")
+const Wishlist = require("./wishList");
+const myOrder = require("./myOrder")
+  
 router.use('/auth',auth)
-router.use('/course',course)
-
+router.use('/Store',Store) 
+router.use('/Address',Address)
+router.use('/Product',Product)
+router.use('/Price',Price)
+router.use('/Cart',Cart)
+router.use('/Wishlist',Wishlist)
+router.use('/myOrder',myOrder)
 
 module.exports = router;
